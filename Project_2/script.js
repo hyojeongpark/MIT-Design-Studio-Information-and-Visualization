c32a3a96bc04b36ab962c508756d25c4fd03ad70
hpark <hpark11@wellesley.edu>
2018-03-04 03:09:46 -0500

    fixed some stuff

--- Project_2/script.js
+++ Project_2/script.js
@@ -1,15 +1,6 @@
 //plots
 
 
-<<<<<<< HEAD
-// var url = 'https://api.darksky.net/forecast/c6b293fcd2092b65cfb7313424b2f7ff/42.361145,-71.057083'
-
-d3.json("data/boston_weather.json",draw);
-
-function draw(error,data){
-
-
-=======
 //var url = 'https://api.darksky.net/forecast/c6b293fcd2092b65cfb7313424b2f7ff/42.361145,-71.057083'
 var mobile1 = d3.select('#mobile1')
 d3.json("data/boston_weather.json", draw);
@@ -38,5 +29,4 @@ function draw(error, data) {
     function farToCelc(fahrenheit) {
         return (parseFloat(fahrenheit) - 32) * 0.5556;
     }
->>>>>>> project-2
 }
