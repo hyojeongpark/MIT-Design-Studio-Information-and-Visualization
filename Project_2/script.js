602edede375878e601ee240b0a9d7d39a8e288f5
hpark <hpark11@wellesley.edu>
2018-03-07 02:27:56 -0500

    added cloudy weather

--- Project_2/script.js
+++ Project_2/script.js
@@ -45,12 +45,14 @@ function draw(error, data) {
     d3.select('#timePlus1').text(nowTime.getHours() + 1 + ':00');
     d3.select('#timePlus2').text(nowTime.getHours() + 2 + ':00');
 
-    addWeatherIcon('#weather-icon-now', data.hourly.icon);
-    addWeatherIcon('#weather-icon-hrBefore2', data.hourly.data[0].icon);
-    addWeatherIcon('#weather-icon-hrBefore', data.hourly.data[1].icon);
-//    addWeatherIcon('#weather-icon-hrBefore', getIcon(nowTime.setHours(nowTime.getHours() - 1), data));
-    addWeatherIcon('#weather-icon-hrAfter', data.hourly.data[9].icon);
-    addWeatherIcon('#weather-icon-hrAfter2', data.hourly.data[12].icon);
+    addWeatherIcon('#weather-icon-now', data.hourly);
+    addWeatherIcon('#weather-icon-hrBefore2', data.hourly.data[0]);
+    addWeatherIcon('#weather-icon-hrBefore', data.hourly.data[1]);
+    //    addWeatherIcon('#weather-icon-hrBefore', getIcon(nowTime.setHours(nowTime.getHours() - 1), data));
+    addWeatherIcon('#weather-icon-hrAfter', data.hourly.data[9]);
+    addWeatherIcon('#weather-icon-hrAfter2', data.hourly.data[12]);
+
+    setBackground('cloudy');
 }
 
 function getIcon(time, data) {
@@ -63,24 +65,37 @@ function getIcon(time, data) {
     return icon;
 }
 
-function addWeatherIcon(dom, icon) {
-    console.log(icon);
-    if (icon === "snow") {
+function setBackground(icon) {
+    if (icon == 'cloudy' || 'snow') {
+        d3.select('#mobile1').style('background', "url('foggy.png'), #000000").style('background-blend-mode', "hard-light");
+
+        d3.select('.dome').style('opacity', 0.88);
+        d3.select('#sun').style('opacity', 0);
+
+        d3.select('.charles').style('background', 'linear-gradient(rgba(14, 30, 75, 0.88), rgba(138, 156, 212, 0.88))').style('mix-blend-mode', 'multiply');
+    }
+}
+
+function addWeatherIcon(dom, node) {
+    console.log(node);
+    if (node.icon === "snow") {
         d3.select(dom).append("img")
             .attr("src", "snow.svg")
             .attr("width", 21)
             .attr("height", 21);
-    } else if (icon === "cloudy") {
+    } else if (node.icon === "cloudy") {
         d3.select(dom).append("img")
             .attr("src", "cloudy.svg")
             .attr("width", 34)
             .attr("height", 21);
-    } else if (icon.includes('partly-cloudy')) {
+    } else if (node.icon.includes('partly-cloudy')) {
         d3.select(dom).append("img")
             .attr("src", "partlycloudy.svg")
             .attr("width", 34)
             .attr("height", 21);
     }
+
+    d3.select(dom).select('.temperature').text(node.temperature);
 }
 
 var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
